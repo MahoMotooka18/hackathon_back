b6462f0dda364d6e5bca8d723e503e2c998d78b6
Maho Motooka <motooka-maho467@g.ecc.u-tokyo.ac.jp>
2023-11-12 14:09:59 +0900

    caseの設定

--- hackathon/knowledge_controller/knowledge_register_controller.go
+++ hackathon/knowledge_controller/knowledge_register_controller.go
@@ -28,74 +28,84 @@ func KnowlegdePostHandler(w http.ResponseWriter, r *http.Request) {
 		Category   string `json:"category"`
 		Curriculum string `json:"curriculum"`
 	}
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&requestBody); err != nil {
-		log.Printf("fail:request body is empty , %v\n", err)
+	switch r.Method {
+	case http.MethodOptions:
+		w.WriteHeader(http.StatusOK)
+		return
+	case http.MethodPost:
+
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&requestBody); err != nil {
+			log.Printf("fail:request body is empty , %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if requestBody.Name == "" {
+			log.Printf("エラー: 名前が入力されていません , \n")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if len(requestBody.Name) > 50 {
+			log.Printf("名前の文字数が50を超えています , \n")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if requestBody.Url == "" {
+			log.Printf("urlが入力されていません, \n")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if requestBody.Category == "" {
+			log.Printf("カテゴリーが選択されていません, \n")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if requestBody.Curriculum == "" {
+			log.Printf("カリキュラムが選択されていません, \n")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		ulidId := generateULID()
+		tx, err := dao.DB.Begin()
+		if err != nil {
+			tx.Rollback()
+			log.Printf("情報の取得に失敗しました, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		_, err = tx.Exec("INSERT INTO knowledge(id, name, url, category, curriculum) VALUES (?,?,?,?,?)", ulidId, requestBody.Name, requestBody.Url, requestBody.Category, requestBody.Curriculum)
+		if err != nil {
+			tx.Rollback()
+			log.Printf("情報の登録に失敗しました , %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := tx.Commit(); err != nil {
+			tx.Rollback()
+			log.Printf("fail:コミットに失敗しました , %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		responseJSON := map[string]string{"id": ulidId.String()}
+		jsonResponse, err := json.Marshal(responseJSON)
+		if err != nil {
+			log.Printf("Marshalingに失敗しました , %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonResponse)
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	if requestBody.Name == "" {
-		log.Printf("エラー: 名前が入力されていません , \n")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if len(requestBody.Name) > 50 {
-		log.Printf("名前の文字数が50を超えています , \n")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if requestBody.Url == "" {
-		log.Printf("urlが入力されていません, \n")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if requestBody.Category == "" {
-		log.Printf("カテゴリーが選択されていません, \n")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if requestBody.Curriculum == "" {
-		log.Printf("カリキュラムが選択されていません, \n")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	ulidId := generateULID()
-	tx, err := dao.DB.Begin()
-	if err != nil {
-		tx.Rollback()
-		log.Printf("情報の取得に失敗しました, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	_, err = tx.Exec("INSERT INTO knowledge(id, name, url, category, curriculum) VALUES (?,?,?,?,?)", ulidId, requestBody.Name, requestBody.Url, requestBody.Category, requestBody.Curriculum)
-	if err != nil {
-		tx.Rollback()
-		log.Printf("情報の登録に失敗しました , %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if err := tx.Commit(); err != nil {
-		tx.Rollback()
-		log.Printf("fail:コミットに失敗しました , %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	responseJSON := map[string]string{"id": ulidId.String()}
-	jsonResponse, err := json.Marshal(responseJSON)
-	if err != nil {
-		log.Printf("Marshalingに失敗しました , %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonResponse)
-
 }
--- hackathon/knowledge_controller/knowledge_search_controller.go
+++ hackathon/knowledge_controller/knowledge_search_controller.go
@@ -14,34 +14,43 @@ func KnowlegdeGetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
-	rows, err := dao.DB.Query("SELECT id, name, url, category, curriculum FROM knowledge")
-	if err != nil {
-		log.Printf("fail: データを持ってくるのに失敗, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
+	switch r.Method {
+	case http.MethodOptions:
+		w.WriteHeader(http.StatusOK)
 		return
-	}
-
-	knowledge := make([]model.KnowledgeResForHTTPGet, 0)
-	for rows.Next() {
-		var u model.KnowledgeResForHTTPGet
-		if err := rows.Scan(&u.Id, &u.Name, &u.Url, &u.Category, &u.Curriculum); err != nil {
-			log.Printf("fail: rows.Scan, %v\n", err)
-			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-				log.Printf("fail: rows.Close(), %v\n", err)
-			}
+	case http.MethodPost:
+		rows, err := dao.DB.Query("SELECT id, name, url, category, curriculum FROM knowledge")
+		if err != nil {
+			log.Printf("fail: データを持ってくるのに失敗, %v\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		knowledge = append(knowledge, u)
-	}
-	// ②-4
-	bytes, err := json.Marshal(knowledge)
-	if err != nil {
-		log.Printf("fail: json.Marshal, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
+
+		knowledge := make([]model.KnowledgeResForHTTPGet, 0)
+		for rows.Next() {
+			var u model.KnowledgeResForHTTPGet
+			if err := rows.Scan(&u.Id, &u.Name, &u.Url, &u.Category, &u.Curriculum); err != nil {
+				log.Printf("fail: rows.Scan, %v\n", err)
+				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+					log.Printf("fail: rows.Close(), %v\n", err)
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			knowledge = append(knowledge, u)
+		}
+		// ②-4
+		bytes, err := json.Marshal(knowledge)
+		if err != nil {
+			log.Printf("fail: json.Marshal, %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bytes)
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(bytes)
-
 }
