3ca7ca5e5b783474b2f03043e84d615bfb8bcb47
Maho Motooka <motooka-maho467@g.ecc.u-tokyo.ac.jp>
2023-11-12 15:09:33 +0900

    daoなし

--- hackathon/main.go
+++ hackathon/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"hackathon/dao"
 	"hackathon/knowledge_controller"
 	"log"
@@ -9,6 +11,29 @@ import (
 	"os/signal"
 	"syscall"
 )
+
+var db *sql.DB
+
+func Init() {
+	//mysqlUser := os.Getenv("MYSQL_USER")
+	//mysqlPwd := os.Getenv("MYSQL_PASSWORD")
+	//mysqlHost := os.Getenv("MYSQL_HOST")
+	//mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PASSWORD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	_db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		log.Fatalf("fail: sql.Open, %v\n", err)
+	}
+	if err := _db.Ping(); err != nil {
+		log.Fatalf("fail: _db.Ping, %v\n", err)
+	}
+	db = _db
+}
 
 func main() {
 	//  /knowledgeでリクエストされたら
